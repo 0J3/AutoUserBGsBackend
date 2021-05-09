c4a9f8b798d870509d169cf5636ab67a5d1b3a7d
0J3 <0J3@wafflepowered.com>
2021-05-09 07:41:17 +0200

    e

--- ts/URLChecker.ts
+++ ts/URLChecker.ts
@@ -1,7 +1,17 @@
 import { cannotUseInsecureUrl, cannotUseNonHttpsProtocol } from './Errors';
 import HttpError from './Errors';
 
-const domains = ['cdn.discordapp.com', 'cdn.discord.com'];
+const domains = [
+  'cdn.discordapp.com',
+  'cdn.discord.com',
+  'github.com',
+  'raw.githubusercontent.com',
+  'nora.lgbt',
+  '0j3.github.io',
+  '0j3-2.github.io',
+  'unsplash.com',
+  'i.imgur.com',
+];
 
 const isProhibited = (url: string) => {
   let v = false;
